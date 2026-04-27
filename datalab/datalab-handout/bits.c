dd47dd7e83ccce4277235b97dfeecbe9ed97e37d
wkxu2024 <1147426328@qq.com>
2026-04-27 18:10:12 +0800

    fix(bits): 优化bitXor函数实现

--- datalab/datalab-handout/bits.c
+++ datalab/datalab-handout/bits.c
@@ -142,7 +142,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return ~(~(~(x & y) & x) & ~(~(x & y) & y));
+  return ~((~(x & ~y)) & (~(~x & y)));
 }
 /*
  * tmin - return minimum two's complement integer
